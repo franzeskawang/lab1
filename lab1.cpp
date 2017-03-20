fd1bd2f76de128d465b33c00970d42ef1c8ec918
franzeska <e24056776@gmail.com>
2017-03-20 15:48:06 +0800

    test

---
+++ lab1.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main(int argc,char*arcv[]){
+  int n;
+  cin >> n;
+  cout << n <<" ";
+  while(n!=1){
+    if(n%2==1){
+      n=3*n+1;
+      cout << n <<" ";
+    }
+    else{
+      n=n/2;
+      cout << n <<" ";
+    }
+   }
+  return 0 ;
+}
